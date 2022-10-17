0a87656b7602da9e6c84191afbaf4d052ee9b363
aftandilmmd <aftandil.mmd@gmail.com>
2022-10-17 11:20:13 +0300

    create terminal layout file

---
+++ src/layouts/TerminalLayout.vue
@@ -0,0 +1,8 @@
+<template>
+  <q-layout view="hHh lpR fFf" class="overflow-hidden">
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+<script setup></script>
