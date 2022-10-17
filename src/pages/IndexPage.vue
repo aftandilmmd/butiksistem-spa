0bef65ac3a70d73de2befe0ad891d15300a174d2
aftandilmmd <aftandil.mmd@gmail.com>
2022-10-17 11:26:36 +0300

    update default content

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,42 +1,19 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class='flex flex-center'>
+    <h5 class='font-light'>Anasayfa</h5>
   </q-page>
 </template>
 
-<script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { ref } from 'vue';
+<script>
+export default {
+  name: 'IndexPage',
+};
+</script>
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
-const meta = ref<Meta>({
-  totalCount: 1200
+<script setup>
+import { useMeta } from 'quasar';
+
+useMeta({
+  title: 'Anasayfa',
 });
 </script>
