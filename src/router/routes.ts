42fc3d2bc896fe11ac25fb8000a701bf591a2c42
aftandilmmd <aftandil.mmd@gmail.com>
2022-10-17 11:22:34 +0300

    add routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,15 +3,43 @@ import { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    component: () => import('src/layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('src/pages/IndexPage.vue') }],
+  },
+
+  {
+    path: '/terminal',
+    component: () => import('src/layouts/TerminalLayout.vue'),
+    children: [
+      {
+        path: 'menu',
+        component: () => import('src/pages/terminal/MenuPage.vue'),
+      },
+      {
+        path: 'sale',
+        component: () => import('src/pages/terminal/SalePage.vue'),
+      },
+      {
+        path: 'park',
+        component: () => import('src/pages/terminal/ParkPage.vue'),
+      },
+      {
+        path: 'history',
+        component: () => import('src/pages/terminal/HistoryPage.vue'),
+      },
+
+      {
+        path: 'return',
+        component: () => import('src/pages/terminal/ReturnPage.vue'),
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    component: () => import('src/pages/ErrorNotFound.vue'),
   },
 ];
 
