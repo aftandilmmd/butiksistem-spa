4aa5171ca31d23701252ccb169582b07d74e798c
aftandilmmd <aftandil.mmd@gmail.com>
2022-10-25 15:16:23 +0300

    refactor: use new model syntax

--- src/components/terminal/product/ProductGridItem.vue
+++ src/components/terminal/product/ProductGridItem.vue
@@ -1,26 +1,26 @@
 <template>
 
-  <div :title="getProductName(product)" class="group relative cursor-pointer rounded shadow transition-all duration-200">
+  <div :title="$(product).getName()" class="group relative cursor-pointer rounded shadow transition-all duration-200">
 
     <div class="group relative w-full h-60 rounded overflow-hidden">
 
       <div @click="selectProduct(product)" class="duration-100 opacity-0 group-hover:opacity-100 bg-blue-300/50 absolute left-0 top-0 right-0 bottom-0"></div>
 
-      <div v-if="hasProductQuantity(product)" @click="selectProduct(product)" class="flex flex-center -mt-12 bg-gray-400/60 absolute left-0 top-0 right-0 bottom-0" >
+      <div v-if="$(product).hasQuantity()" @click="selectProduct(product)" class="flex flex-center -mt-12 bg-gray-400/60 absolute left-0 top-0 right-0 bottom-0" >
 
-        <q-btn color="white" text-color="grey-9">{{ getProductQuantity(product) }} Adet</q-btn>
+        <q-btn color="white" text-color="grey-9">{{ $(product).getQuantity() }} Adet</q-btn>
 
       </div>
 
       <img
         @click="selectProduct(product)"
-        :alt="getProductName(product)"
+        :alt="$(product).getName()"
         src="https://images.pexels.com/photos/13428312/pexels-photo-13428312.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
         class="w-full h-full object-center object-cover bg-white"
       />
 
-      <span v-if="! hasProductMultipleVariants(product)" class="absolute left-0 top-0 m-2 bg-blue-500 text-white font-normal text-sm rounded px-2">
-        {{ getVariantName( getProductFirstVariant(product) ) }}
+      <span v-if="! $(product).hasManyVariants()" class="absolute left-0 top-0 m-2 bg-blue-500 text-white font-normal text-sm rounded px-2">
+        {{ $($(product).getFirstVariant()).getName() }}
       </span>
 
       <q-btn unelevated icon="more_vert" size="sm" class="text-xs absolute px-2 right-0 top-0 m-2">
@@ -62,18 +62,18 @@
       <div @click="selectProduct(product)" class="absolute overflow-hidden w-full px-4 py-3 bottom-0 bg-white flex flex-col">
 
         <h3 class="w-full truncate overflow-hidden text-sm font-bold text-gray-800 m-0">
-          {{ getProductName(product) }}
+          {{ $(product).getName() }}
         </h3>
 
         <p class="m-0 mt-1 text-sm text-gray-500 font-medium">
-          {{ Money(getProductPrice(product)) }}
+          {{ Money($(product).getPrice()) }}
         </p>
 
       </div>
 
     </div>
 
-    <div v-if="hasProductMultipleVariants(product)" class="group-hover:mt-[0.65rem] group-hover:shadow-md transition-all duration-200 absolute top-0 h-full left-[0.35rem] right-[0.35rem] bg-white shadow mt-[0.35rem] rounded -z-[2]"></div>
+    <div v-if="$(product).hasManyVariants()" class="group-hover:mt-[0.65rem] group-hover:shadow-md transition-all duration-200 absolute top-0 h-full left-[0.35rem] right-[0.35rem] bg-white shadow mt-[0.35rem] rounded -z-[2]"></div>
 
   </div>
 
@@ -91,39 +91,21 @@
 
 </template>
 
-<script>
+<script setup lang="ts">
 import ProductVariantDialog from 'src/components/terminal/product/dialog/ProductVariantDialog.vue';
 import ProductStockDialog from 'src/components/terminal/product/dialog/ProductStockDialog.vue';
-</script>
+import { ProductInterface, VariantInterface, CartItemInterface } from 'src/core/types/model';
 
-<script setup>
 
 import { reactive, ref } from 'vue';
-import { Money } from 'src/utils/Money';
 import { useProductStore } from 'src/stores/terminal/product-store';
 import { useCartStore } from 'src/stores/terminal/cart-store';
 
-import {
-	getProductName,
-	getProductPrice,
-	getProductQuantity,
-	getProductFirstVariant,
+import { Money } from 'src/utils/Money';
+import $ from 'src/core/models/Model'
 
-	getVariantName,
-  hasProductQuantity,
-	hasVariantQuantity,
-  hasProductMultipleVariants,
-  newProductWithVariant,
-} from 'src/resources/Product';
-
-
-let emit  = defineEmits(['select-variant', 'show-stock']);
-let props = defineProps({
-  product: {
-    type: Object,
-    default: () => ({}),
-  },
-});
+const props = defineProps<{ product: ProductInterface }>()
+const emit  = defineEmits<{ (e: 'select-variant', value: ProductInterface | CartItemInterface): void }>()
 
 const productStore = useProductStore();
 const cartStore    = useCartStore();
@@ -133,24 +115,24 @@ const dialogs = reactive({
   stock  : false,
 })
 
-const stockProduct = ref(false);
+const stockProduct = ref<ProductInterface>();
 
-function selectProduct(product) {
+function selectProduct(product: ProductInterface): void {
 
-  if ( hasProductMultipleVariants(product) ) {
+  if ( $(product).hasManyVariants() ) {
     dialogs.variant = true;
     return;
   }
 
-  selectVariant( getProductFirstVariant(product) );
+  selectVariant( $(product).getFirstVariant() );
 
 }
 
-function selectVariant(variant) {
+function selectVariant(variant: VariantInterface): void {
 
-  if (! hasVariantQuantity(variant) ) return;
+  if (! $(variant).hasQuantity() ) return;
 
-  let cart_item = newProductWithVariant(props.product, variant);
+  let cart_item = $(props.product).create(variant);
 
   cartStore.add(cart_item)
 
@@ -158,12 +140,12 @@ function selectVariant(variant) {
 
 }
 
-function showStock(product) {
+function showStock(product: ProductInterface) {
   stockProduct.value = product;
   dialogs.stock = true;
 }
 
-function favorite(product) {
+function favorite(product: ProductInterface) {
 
   if (isFavorited(product.id)) {
     productStore.unfavorite(product.id);
@@ -173,7 +155,7 @@ function favorite(product) {
   productStore.favorite(product);
 }
 
-function isFavorited(product_id) {
+function isFavorited(product_id: number) {
   return productStore.isFavorited(product_id);
 }
 
