6ab15564d138fbb6b71f9005b2f2368c6496f43f
aftandilmmd <aftandil.mmd@gmail.com>
2022-10-27 12:38:59 +0300

    refactor getters and much more

--- src/components/terminal/cart/dialog/CartItemDetailDialog.vue
+++ src/components/terminal/cart/dialog/CartItemDetailDialog.vue
@@ -2,7 +2,7 @@
   <q-card class="flex flex-col">
     <div class="w-full bg-[#002640] p-6">
       <h1 class="m-0 text-xl text-white mx-auto font-normal">
-        {{ getProductName(item) }}
+        {{ Product(item).getName() }}
       </h1>
     </div>
 
@@ -19,6 +19,7 @@
 
         <input
           v-model="variant_form.attributes.quantity"
+          @blur="checkQuantity"
           class="w-full rounded p-3 py-2 text-3xl border-2 border-solid border-gray-300 focus:border-gray-400 text-center"
         />
 
@@ -127,15 +128,16 @@
 
       </div>
 
-      <div v-if="has_discount" class="absolute left-0 top-0 right-0 bottom-0 bg-white flex flex-center">
+      <div v-if="is_price_updated" class="absolute left-0 top-0 right-0 bottom-0 bg-white flex flex-center">
 
-        <q-btn @click="removeDiscount" :label="remove_meta_price_message" text-color="grey-10" color="white" class="py-3" />
+        <q-btn @click="removeDiscount" :label="discount_remove_message" text-color="grey-10" color="white" class="py-3" />
 
       </div>
 
     </q-card-section>
 
     <dialog-card-actions
+      cancel-button-text="İptal"
       confirm-button-text="Uygula"
       @confirm="update"
       :disabled="is_over_discounted"
@@ -144,69 +146,42 @@
 </template>
 
 <script setup lang="ts">
-
 import DialogCardActions from 'src/components/terminal/_shared/dialog/DialogCardActions.vue';
 
-import { ref, computed, onBeforeMount } from 'vue';
+import { ref, computed } from 'vue';
+import { CartItemInterface, DiscountType, TransactionType, VariantInterface } from 'src/core/types/model'
 import { cloneDeep } from 'lodash';
 import { useCartStore } from 'src/stores/terminal/cart-store';
-import { useQuasar } from 'quasar';
 import { Money } from 'src/utils/Money';
-import { CartItemInterface, DiscountType, TransactionType } from 'src/core/types/model'
+import { positiveNotify } from 'src/utils/Notify';
 
-import {
-  isCartItemPriceUpdated,
-  isCartItemVariantOverDiscounted,
-  getCartItemVariantMetaAmount,
-  removeCartItemDiscount,
-  setCartItemVariant,
-} from 'src/resources/Cart';
-
-import { getProductName, getProductFirstVariant, getVariantQuantity } from 'src/resources/Product';
-
-const cartStore = useCartStore();
-const $q = useQuasar();
+import CartItem from 'src/core/models/CartItem';
+import Variant from 'src/core/models/Variant';
+import Product from 'src/core/models/Product';
 
 const props = defineProps<{ item: CartItemInterface }>()
 const emit  = defineEmits<{ (e: 'update', value: CartItemInterface): void }>()
 
-let updated_item   = cloneDeep(props.item);
-const variant_form = ref({
-  type  : 'variants',
-  id    : 0,
+const cartStore    = useCartStore();
+const item_variant = cloneDeep(CartItem(props.item).getVariant());
+let variant_form   = ref<VariantInterface>(item_variant);
 
-  attributes:{
-    name     : '',
-    price    : 0,
-    quantity : 0,
-  },
-
-  meta:{
-    hash_id: '',
-    transaction_type: 'custom',
-    discount_type: 'percent',
-    amount: null
-  }
-});
-
-onBeforeMount(() => {
-  variant_form.value = JSON.parse(JSON.stringify(getProductFirstVariant(props.item)));
-});
-
-let has_discount = computed(() => isCartItemPriceUpdated(props.item) );
-let amount       = computed(() => getCartItemVariantMetaAmount(props.item) );
-let is_over_discounted = computed(() => isCartItemVariantOverDiscounted(props.item, variant_form.value) );
+let amount = computed(() => CartItem(props.item).getAmount() );
+let is_price_updated = computed(() => CartItem(props.item).isPriceUpdated() );
+let is_over_discounted = computed(() => isOverDiscounted() );
 
 let over_discount_message = 'Uyarı! Ürün fiyatı eksiye düştü.';
+let discount_remove_message = computed(() => {
 
-let remove_meta_price_message = computed(() => {
-return variant_form.value.meta.transaction_type === 'custom' ?
-      `${Money(amount.value)} ÖZEL FİYATI KALDIR.` :
-      `${amount.value}${ variant_form.value.meta.discount_type === 'fixed' ? '₺' : '%' } İNDİRİMİ KALDIR.`;
+  if(Variant(variant_form.value).isTransactionCustom()){
+    return `${Money(amount.value)} ÖZEL FİYATI KALDIR.`;
+  }
+
+  return `${amount.value}${ Variant(variant_form.value).isDiscountFixed() ? '₺' : '%' } İNDİRİMİ KALDIR.`;
 });
 
 function decQuantity(): void {
-  if ( getVariantQuantity(variant_form.value) > 1){
+  if ( Variant(variant_form.value).getQuantity() > 1){
     variant_form.value.attributes.quantity--;
   }
 }
@@ -224,25 +199,48 @@ function setDiscountType(type: DiscountType): void {
 }
 
 function removeDiscount(): void {
-  variant_form.value = removeCartItemDiscount( props.item ).relations.variants[0]
+  variant_form.value = CartItem(CartItem(props.item).removeDiscount()).getVariant();
 }
 
 function update(): void {
 
   if (is_over_discounted.value) return;
 
-  updated_item = setCartItemVariant(updated_item, variant_form.value);
+  const item = CartItem(cloneDeep(props.item)).setVariant(variant_form.value);
 
-  cartStore.update(updated_item);
+  cartStore.update(item);
 
-  emit('update', updated_item);
+  emit('update', item);
 
-  $q.notify({
-    icon   : 'check',
-    type   : 'positive',
-    message: 'Sepet güncellendi.',
-  });
+  positiveNotify('Sepet güncellendi.')
 
+}
+
+function isOverDiscounted(): boolean {
+
+  let price = Variant(variant_form.value).getPrice() ?? Product(props.item).getPrice()
+
+  if (Variant(variant_form.value).getTransactionType() === 'custom') {
+    return Variant(variant_form.value).getAmount() < 0;
+  }
+
+  if (Variant(variant_form.value).getDiscountType() === 'percent' && Variant(variant_form.value).getAmount() > 100) {
+    return true;
+  }
+
+  if ( Variant(variant_form.value).getDiscountType() == 'fixed' && Variant(variant_form.value).getAmount() > price ) {
+    return true;
+  }
+
+  return false;
+
+}
+
+function checkQuantity(){
+  let qty = variant_form.value.attributes.quantity;
+  if(qty < 1 || qty > 10000){
+    variant_form.value.attributes.quantity = 1;
+  }
 }
 
 </script>
