41d9dacf6e832112b2349fb3383b464087cf539c
aftandilmmd <aftandil.mmd@gmail.com>
2022-11-01 13:18:04 +0300

    refactor CartParkDialog

--- src/components/terminal/sidebar/panel/action/dialog/CartParkDialog.vue
+++ src/components/terminal/sidebar/panel/action/dialog/CartParkDialog.vue
@@ -1,81 +1,48 @@
 <template>
   <q-card class="flex flex-col w-full xl:w-6/12">
-    <div class="w-full bg-[#002640] p-6" align="center">
-      <h1 class="m-0 text-xl text-white mx-auto font-normal uppercase">
-        SATIŞI PARK ET
-      </h1>
-    </div>
 
-    <q-card-section
-      class="px-8 py-16 w-full q-pt-none flex-1 flex flex-center mt-3"
-    >
+    <dialog-card-header>SATIŞI PARK ET</dialog-card-header>
+
+    <q-card-section class="px-8 py-16 w-full q-pt-none flex-1 flex flex-center mt-3">
       <div class="relative w-full flex flex-row items-center gap-5">
         <q-input
           v-model="title"
           ref="parkInputRef"
-          autogrow
           outlined
           class="flex-1 text-right text-xl leading-relaxed"
-          placeholder="Park Notu"
+          placeholder="Park başlığı"
         />
       </div>
     </q-card-section>
 
-    <q-card-actions align="right" class="p-8 py-5 bg-gray-100">
-      <q-btn
-        flat
-        label="İptal"
-        color="grey-7"
-        padding="8px 2rem"
-        v-close-popup
-      />
-      <q-btn
-        @click="parkCart"
-        unelevated
-        label="Park Et"
-        color="dark"
-        padding="8px 2rem"
-        v-close-popup
-      />
-    </q-card-actions>
+    <dialog-card-actions @confirm="parkCart" />
+
   </q-card>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
-import { useQuasar } from 'quasar';
-import { useParkStore } from 'src/stores/terminal/park-store';
-import { useCartStore } from 'src/stores/terminal/cart-store';
-import { v4 as uuidv4 } from 'uuid';
-import * as dayjs from 'dayjs';
+import { useCart } from 'src/core/composables/useCart';
+import { positiveNotify } from 'src/utils/Notify';
+import DialogCardActions from 'src/components/terminal/_shared/dialog/DialogCardActions.vue';
+import DialogCardHeader from 'src/components/terminal/_shared/dialog/DialogCardHeader.vue';
 
 const emit = defineEmits(['parked']);
 
-const $q = useQuasar();
 const title = ref('');
 const parkInputRef = ref(null);
-const parkStore = useParkStore();
-const cartStore = useCartStore();
+const CartManager = useCart();
 
 function parkCart() {
-  let park = {
-    title: title.value,
-    cart: cartStore.parkState,
-    hash_id: uuidv4(),
-    parked_at: dayjs().format(),
-  };
 
-  parkStore.addPark(park);
+  const park = CartManager.parkCurrentCart(title.value);
 
-  emit('parked', park.hash_id);
+  emit('parked', park.id);
 
-  cartStore.reset();
+  CartManager.clearItems();
 
-  $q.notify({
-    type: 'positive',
-    icon: 'check',
-    message: 'Sepetiniz park edildi.',
-  });
+  positiveNotify('Sepetiniz park edildi.');
+
 }
 
 onMounted(() => {
