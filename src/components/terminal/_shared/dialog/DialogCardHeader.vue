d854394319051d62fd7c511bfe4bec8999ff2eb1
aftandilmmd <aftandil.mmd@gmail.com>
2022-11-01 13:17:37 +0300

    create DialogCardHeader component

---
+++ src/components/terminal/_shared/dialog/DialogCardHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-full bg-[#002640] p-6" align="center">
+    <h1 class="m-0 text-xl text-white mx-auto font-normal uppercase">
+      <slot></slot>
+    </h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
