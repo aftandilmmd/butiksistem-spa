02f9dbe3fe9ccda9d86b3f70fe01281c66e27fd6
aftandilmmd <aftandil.mmd@gmail.com>
2022-11-09 14:13:24 +0300

    create panel wrapper component

---
+++ src/components/terminal/history/components/PanelWrapper.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-full rounded-md bg-white p-5 shadow-md mb-5">
+    <span class="block font-medium text-gray-700 text-sm mb-4">
+      <slot name="title"></slot>
+    </span>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
