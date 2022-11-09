465da2eb48ce3b21b3beaa87f474da941347990b
aftandilmmd <aftandil.mmd@gmail.com>
2022-11-09 16:32:13 +0300

    create empty cart message

---
+++ src/components/terminal/cart/EmptyCartMessage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex items-center justify-center text-center flex-1">
+    <div class="text-lg font-light text-gray-400">
+      <p class="m-0 p-0">Satışa başlamak için sepete ürün ekle.</p>
+      <p class="m-0 p-0">Hedefine çok az kaldı 👍</p>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
